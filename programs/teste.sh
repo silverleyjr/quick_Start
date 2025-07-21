a58d4e87b25dee0fe89d5b16fe96c5c225a9ceee
silverley <silverleyjr@gmail.com>
2025-07-21 14:38:52 -0300

    melhor forma para o bin no .local, removendo espaços extras

--- programs/teste.sh
+++ programs/teste.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 git clone git@github.com:silverleyjr/dotfiles.git
+sudo apt install curl
 sudo apt install zsh
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz 
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 cp ./dotfiles/.zshrc ./
 cp ./dotfiles/nvim ./.config
-cd .local
-mkdir bin
-cd
+mkdir ./.local/bin
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
     nvm install v23.9.0
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
