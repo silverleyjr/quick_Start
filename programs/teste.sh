38e0cd53a998571e2536fc2fa7a3052be8d649de
silverley <silverleyjr@gmail.com>
2025-07-21 13:41:50 -0300

    tirando kitty e arrumando o local do zshrc e nvim

--- programs/teste.sh
+++ programs/teste.sh
@@ -1,24 +1,15 @@
 git clone git@github.com:silverleyjr/dotfiles.git
 sudo apt install zsh
-cp /dotfiles/.zshrc ./
+cp ./dotfiles/.zshrc ./
 sudo snap install nvim --classic.
-cp /dotfiles/nvim /.config
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+cp ./dotfiles/nvim ./.config
 cd .local
 mkdir bin
 cd
-ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
     nvm install v23.9.0
-gsettings set org.gnome.desktop.default-applications.terminal exec 'kitty'
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $(which kitty) 50
-gsettings set org.gnome.desktop.default-applications.terminal exec $(which kitty)
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
